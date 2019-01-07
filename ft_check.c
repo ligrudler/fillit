bda717b35c72368c36287c441cc42c9cbcaa51f9
Edward LINDAO MARAZITA <elindao-@e2r3p2.42.fr>
2019-01-07 16:29:56 +0100

    modif 1

--- ft_check.c
+++ ft_check.c
@@ -3,13 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   ft_check.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgrudler <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: elindao- <elindao-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 16:13:49 by lgrudler          #+#    #+#             */
-/*   Updated: 2019/01/07 16:22:47 by lgrudler         ###   ########.fr       */
+/*   Updated: 2019/01/07 16:29:13 by elindao-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-include "fillit.h"
+#include "fillit.h"
 
+int		puterror(void)
+{
+	write(2, "error\n", 6);
+	return (0);
+}
 
+int		good_pattern(char *str)
+{
+	int		i;
+	int		dot;
+	int		hashtag;
+
+	i = 0;
+	dot = 0;
+	hashtag = 0;
+	while(str[i] && i < 21)
+	{
+		if (str[i] != '.' && str[i] != '#' && str[i] != '\n')
+		return (0);
+		if (str[i] == '.')
+		dot++;
+		if (str[i] == '#')
+		hashtag++;
+		i++;
+	}
+	if (hashtag == 4 && dot == 12)
+		return (1);
+	return (0);
+}
