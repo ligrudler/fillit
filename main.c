7e0c8dffc4e430866e878f73a6d3a4ef74d33c50
Lise Grudler <lgrudler@student.42.fr>
2019-01-08 16:01:11 +0100

    tentative resolution pb ;uavais ficheier

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: elindao- <elindao-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 19:44:40 by elindao-          #+#    #+#             */
-/*   Updated: 2019/01/08 14:58:12 by elindao-         ###   ########.fr       */
+/*   Updated: 2019/01/08 15:46:20 by lgrudler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int		main(int ac, char **av)
 	fd = open(av[1], O_RDONLY);
 	ret = read(fd, str, 548);
 	str[ret] = '\0';
-	if (check_file(str) == 1)
+	if (check_file(str)== 1)
 		ft_putendl("Bon Fichier !");
 	else
 		ft_putendl("Mauvais fichier !");
