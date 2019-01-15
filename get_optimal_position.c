994fa9766a0e42722383a4290d164dbc84be00bf
Edward LINDAO MARAZITA <elindao-@e2r3p2.42.fr>
2019-01-15 15:56:17 +0100

    modification de get_optimal_position.c

--- get_optimal_position.c
+++ get_optimal_position.c
@@ -6,7 +6,7 @@
 /*   By: elindao- <elindao-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/10 15:07:55 by elindao-          #+#    #+#             */
-/*   Updated: 2019/01/14 15:34:31 by elindao-         ###   ########.fr       */
+/*   Updated: 2019/01/15 15:55:49 by elindao-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,23 @@ int		check_special_pattern(char *block)
 	i = 0;
 	while (block[i] != '#')
 		i++;
-	if (block[i + 3] == '#' && block[i + 4] == '#' && block[i + 7] == '#')
+	if (block[i + 3] == '#' && block[i + 4] == '#' && block[i + 7] == '#'
+			&& i < 8)
 		return (1);
-	if (block[i + 1] == '#' && block[i + 3] == '#' && block[i + 4] == '#')
+	if (block[i + 1] == '#' && block[i + 3] == '#' && block[i + 4] == '#'
+			&& i < 11)
 		return (1);
-	if (block[i + 4] == '#' && block[i + 7] == '#' && block[i + 8] == '#')
+	if (block[i + 4] == '#' && block[i + 7] == '#' && block[i + 8] == '#'
+			&& i < 8)
 		return (1);
-	if (block[i + 3] == '#' && block[i + 4] == '#' && block[i + 5] == '#')
+	if (block[i + 3] == '#' && block[i + 4] == '#' && block[i + 5] == '#'
+			&& i < 11)
 		return (1);
-	if (block[i + 3] == '#' && block[i + 4] == '#' && block[i + 8] == '#')
+	if (block[i + 3] == '#' && block[i + 4] == '#' && block[i + 8] == '#'
+			&& i < 8)
 		return (1);
-	if (block[i + 2] == '#' && block[i + 3] == '#' && block[i + 4] == '#')
+	if (block[i + 2] == '#' && block[i + 3] == '#' && block[i + 4] == '#'
+			&& i < 12)
 		return (2);
 	return (0);
 }
